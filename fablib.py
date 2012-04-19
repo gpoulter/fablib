8e7204ad7eacd1ba4b33782f829ece1e40033a59
Graham Poulter <graham@mocality.com>
2012-04-19 18:19:14 +0200

    Rearrange fablib.py to create folded sections by topic

--- fablib.py
+++ fablib.py
@@ -4,41 +4,32 @@ from cuisine import (
     package_update_apt, package_upgrade_apt, package_install_apt)
 from fabric.api import (abort, env, get, hide, hosts, lcd, local, put, roles,
                         run, runs_once, settings, sudo)
+from StringIO import StringIO
+from contextlib import contextmanager
 from fabric.contrib.files import exists
 from fabric.contrib.project import rsync_project
 from os.path import dirname, join
-from contextlib import contextmanager
-from StringIO import StringIO
 import os
 import re
 import time
 
-
-def chown(dirs, user=None, group=None):
-    """User sudo to set user and group ownership"""
-    if isinstance(dirs, basestring):
-        dirs = [dirs]
-    args = ' '.join(dirs)
-    if user and group:
-        return sudo('chown {}:{} {}'.format(user, group, args))
-    elif user:
-        return sudo('chown {} {}'.format(user, args))
-    elif group:
-        return sudo('chgrp {} {}'.format(group, args))
-    else:
-        return None
+### {{{ ROLES HELPERS
 
 
-def cron(name, timespec, user, command, environ=None):
-    """Create entry in /etc/cron.d"""
-    path = '/etc/cron.d/{}'.format(name)
-    entry = '{}\t{}\t{}\n'.format(timespec, user, command)
-    if environ:
-        envstr = '\n'.join('{}={}'.format(k, v)
-                           for k, v in environ.iteritems())
-        entry = '{}\n{}'.format(envstr, entry)
-    chput(StringIO(entry), path, use_sudo=True,
-          mode=0o644, user='root', group='root')
+def default_roles(*role_list):
+    """Decorate task with these roles by default, but override with -R, -H"""
+    def selectively_attach(func):
+        """Only decorate if nothing specified on command line"""
+        # pylint: disable=W0142
+        if not env.roles and not env.hosts:
+            return roles(*role_list)(func)
+        else:
+            if env.hosts:
+                func = hosts(*env.hosts)(func)
+            if env.roles:
+                func = roles(*env.roles)(func)
+            return func
+    return selectively_attach
 
 
 def hasrole(role):
@@ -60,6 +51,73 @@ def pickrole(role_list=None, strict=False):
         if strict:
             abort("No role found for host: {}".format(env.host))
 
+### ROLES HELPERS }}}
+### {{{ FILE AND DIRECTORY HELPERS
+
+
+def chown(dirs, user=None, group=None):
+    """User sudo to set user and group ownership"""
+    if isinstance(dirs, basestring):
+        dirs = [dirs]
+    args = ' '.join(dirs)
+    if user and group:
+        return sudo('chown {}:{} {}'.format(user, group, args))
+    elif user:
+        return sudo('chown {} {}'.format(user, args))
+    elif group:
+        return sudo('chgrp {} {}'.format(group, args))
+    else:
+        return None
+
+
+def chput(local_path=None, remote_path=None, user=None, group=None,
+          mode=None, use_sudo=True, mirror_local_mode=False, check=True):
+    """Put file and set user and group ownership.  Default to use sudo."""
+    # pylint: disable=R0913
+    if env.get('full') or not check or diff(local_path, remote_path):
+        result = put(local_path, remote_path, use_sudo,
+                     mirror_local_mode, mode)
+        with hide('commands'):
+            chown(remote_path, user, group)
+        return result
+    else:
+        return None
+
+
+def cron(name, timespec, user, command, environ=None):
+    """Create entry in /etc/cron.d"""
+    path = '/etc/cron.d/{}'.format(name)
+    entry = '{}\t{}\t{}\n'.format(timespec, user, command)
+    if environ:
+        envstr = '\n'.join('{}={}'.format(k, v)
+                           for k, v in environ.iteritems())
+        entry = '{}\n{}'.format(envstr, entry)
+    chput(StringIO(entry), path, use_sudo=True,
+          mode=0o644, user='root', group='root')
+
+
+def diff(local_path, remote_path):
+    """Return true if local and remote paths differ in contents"""
+    with hide('commands'):
+        if isinstance(local_path, basestring):
+            local_content = local("cat '{}'".format(local_path), capture=True)
+        else:
+            pos = local_path.tell()
+            local_content = local_path.read()
+            local_path.seek(pos)
+        remote_content = StringIO()
+        with settings(hide('warnings'), warn_only=True):
+            if get(remote_path, remote_content).failed:
+                return True
+        return local_content.strip() != remote_content.getvalue().strip()
+
+
+def md5sum(filename, use_sudo=False):
+    """Return md5sum of remote file"""
+    runner = sudo if use_sudo else run
+    with hide('commands'):
+        return runner("md5sum '{}'".format(filename)).split()[0]
+
 
 def mkdir(dirs, user=None, group=None, mode=None, use_sudo=True):
     """Create directory with sudo and octal mode, then set ownership."""
@@ -79,57 +137,58 @@ def mkdir(dirs, user=None, group=None, mode=None, use_sudo=True):
         return None
 
 
-def chput(local_path=None, remote_path=None, user=None, group=None,
-          mode=None, use_sudo=True, mirror_local_mode=False, check=True):
-    """Put file and set user and group ownership.  Default to use sudo."""
-    # pylint: disable=R0913
-    if env.get('full') or not check or diff(local_path, remote_path):
-        result = put(local_path, remote_path, use_sudo,
-                     mirror_local_mode, mode)
-        with hide('commands'):
-            chown(remote_path, user, group)
-        return result
-    else:
-        return None
+def rsync(local_path, remote_path, exclude=None):
+    """Helper to rsync submodules across"""
+    if not local_path.endswith('/'):
+        local_path += '/'
+    exclude = exclude or []
+    exclude.extend(['*.egg-info', '*.pyc', '/build/', '/dist/'])
+    with hide('running'):
+        run("mkdir -p '{}'".format(remote_path))
+        return rsync_project(
+            remote_path, local_path, delete=True,
+            extra_opts='-i --omit-dir-times -FF --quiet',
+            ssh_opts='-o StrictHostKeyChecking=no',
+            exclude=exclude)
 
+
+def rsync_module(module, exclude=None):
+    """Rsync a dir from modules directory to remote code directory"""
+    rsync(join('modules', module), join(env.code, module), exclude)
+
+
+@contextmanager
+def tempput(local_path=None, remote_path=None, use_sudo=False,
+           mirror_local_mode=False, mode=None):
+    """Put a file to remote and remove it afterwards"""
+    import warnings
+    warnings.simplefilter('ignore', RuntimeWarning)
+    if remote_path is None:
+        remote_path = os.tempnam()
+    put(local_path, remote_path, use_sudo, mirror_local_mode, mode)
+    yield remote_path
+    run("rm '{}'".format(remote_path))
+
+
+@contextmanager
+def watch(filenames, callback, use_sudo=False):
+    """Call callback if any of filenames change during the context"""
+    filenames = [filenames] if isinstance(filenames, basestring) else filenames
+    old_md5 = {fn: md5sum(fn, use_sudo) for fn in filenames}
+    yield
+    for filename in filenames:
+        if md5sum(filename, use_sudo) != old_md5[filename]:
+            callback()
+            return
+
+### FILE AND DIRECTORY HELPERS }}}
+### {{{ DEBIAN/UBUNTU HELPERS
 
 def debconf_set_selections(package, selections):
     """Given package and map config:(type,value), set selections"""
     text = '\n'.join(' '.join([package, k, t, v]) for
                      k, (t, v) in selections.iteritems())
     sudo('debconf-set-selections <<-HEREDOC\n{}\nHEREDOC'.format(text))
-
-
-def default_roles(*role_list):
-    """Decorate task with these roles by default, but override with -R, -H"""
-    def selectively_attach(func):
-        """Only decorate if nothing specified on command line"""
-        # pylint: disable=W0142
-        if not env.roles and not env.hosts:
-            return roles(*role_list)(func)
-        else:
-            if env.hosts:
-                func = hosts(*env.hosts)(func)
-            if env.roles:
-                func = roles(*env.roles)(func)
-            return func
-    return selectively_attach
-
-
-def diff(local_path, remote_path):
-    """Return true if local and remote paths differ in contents"""
-    with hide('commands'):
-        if isinstance(local_path, basestring):
-            local_content = local("cat '{}'".format(local_path), capture=True)
-        else:
-            pos = local_path.tell()
-            local_content = local_path.read()
-            local_path.seek(pos)
-        remote_content = StringIO()
-        with settings(hide('warnings'), warn_only=True):
-            if get(remote_path, remote_content).failed:
-                return True
-        return local_content.strip() != remote_content.getvalue().strip()
 
 
 def install_deb(pkgname, url):
@@ -144,6 +203,35 @@ def install_deb(pkgname, url):
         return True
     else:
         return False
+
+
+def package_ensure_apt(package, update=False):
+    """Ensure apt packages are installed"""
+    if not isinstance(package, basestring):
+        package = " ".join(package)
+    status = run("dpkg-query -W -f='${Status} ' %s ; true" % package)
+    if 'No packages found' in status or 'not-installed' in status:
+        package_install_apt(package)
+        return False
+    else:
+        if update:
+            package_update_apt(package)
+        return True
+
+
+@runs_once
+def update_apt(days=None, upgrade=False):
+    """Update apt index if not update in last N days"""
+    days = (3 if env.get('full') else 14) if days is None else days
+    with hide('commands'):
+        lastupdate = int(run('stat /var/cache/apt -c %Y'))
+    if (time.time() - lastupdate) > days * 86400:
+        package_update_apt()
+        if upgrade:
+            package_upgrade_apt()
+
+### DEBIAN/UBUNTU HELPERS }}}
+### {{{ VERSION TAGGING HELPERS
 
 
 def make_version(ref=None):
@@ -187,38 +275,16 @@ def tagversion(repo, level='patch', special=''):
         local('git tag -s --force v{}'.format(version_string))
 
 
-def package_ensure_apt(package, update=False):
-    """Ensure apt packages are installed"""
-    if not isinstance(package, basestring):
-        package = " ".join(package)
-    status = run("dpkg-query -W -f='${Status} ' %s ; true" % package)
-    if 'No packages found' in status or 'not-installed' in status:
-        package_install_apt(package)
-        return False
-    else:
-        if update:
-            package_update_apt(package)
-        return True
+def write_version(path, ref=None):
+    """Update version file using git desribe"""
+    with lcd(dirname(path)):
+        version = make_version(ref)
+    if env.get('full') or version != open(path).read().strip():
+        with open(path, 'w') as out:
+            out.write(version)
 
-
-def rsync(local_path, remote_path, exclude=None):
-    """Helper to rsync submodules across"""
-    if not local_path.endswith('/'):
-        local_path += '/'
-    exclude = exclude or []
-    exclude.extend(['*.egg-info', '*.pyc', '/build/', '/dist/'])
-    with hide('running'):
-        run("mkdir -p '{}'".format(remote_path))
-        return rsync_project(
-            remote_path, local_path, delete=True,
-            extra_opts='-i --omit-dir-times -FF --quiet',
-            ssh_opts='-o StrictHostKeyChecking=no',
-            exclude=exclude)
-
-
-def rsync_module(module, exclude=None):
-    """Rsync a dir from modules directory to remote code directory"""
-    rsync(join('modules', module), join(env.code, module), exclude)
+### VERSION TAGGING HELPERS }}}
+### {{{ SPLUNK HELPERS
 
 
 def splunk(cmd, user='admin', passwd='changeme'):
@@ -241,55 +307,5 @@ def splunk_monitor(monitors):
             splunk("add monitor '{}' -sourcetype {}".format(path, sourcetype))
             env['splunk_monitors'] += '\n' + path
 
-
-@contextmanager
-def tempput(local_path=None, remote_path=None, use_sudo=False,
-           mirror_local_mode=False, mode=None):
-    """Put a file to remote and remove it afterwards"""
-    import warnings
-    warnings.simplefilter('ignore', RuntimeWarning)
-    if remote_path is None:
-        remote_path = os.tempnam()
-    put(local_path, remote_path, use_sudo, mirror_local_mode, mode)
-    yield remote_path
-    run("rm '{}'".format(remote_path))
-
-
-@runs_once
-def update_apt(days=None, upgrade=False):
-    """Update apt index if not update in last N days"""
-    days = (3 if env.get('full') else 14) if days is None else days
-    with hide('commands'):
-        lastupdate = int(run('stat /var/cache/apt -c %Y'))
-    if (time.time() - lastupdate) > days * 86400:
-        package_update_apt()
-        if upgrade:
-            package_upgrade_apt()
-
-
-def md5sum(filename, use_sudo=False):
-    """Return md5sum of remote file"""
-    runner = sudo if use_sudo else run
-    with hide('commands'):
-        return runner("md5sum '{}'".format(filename)).split()[0]
-
-
-@contextmanager
-def watch(filenames, callback, use_sudo=False):
-    """Call callback if any of filenames change during the context"""
-    filenames = [filenames] if isinstance(filenames, basestring) else filenames
-    old_md5 = {fn: md5sum(fn, use_sudo) for fn in filenames}
-    yield
-    for filename in filenames:
-        if md5sum(filename, use_sudo) != old_md5[filename]:
-            callback()
-            return
-
-
-def write_version(path, ref=None):
-    """Update version file using git desribe"""
-    with lcd(dirname(path)):
-        version = make_version(ref)
-    if env.get('full') or version != open(path).read().strip():
-        with open(path, 'w') as out:
-            out.write(version)
+# SPLUNK HELPERS }}}
+# vim:foldnestmax=1:foldenable:foldmethod=marker:
